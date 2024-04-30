e1651d827dc957c82e29972db6bfa38c1040add4
Mike Holmes <michael@mcholmes.com>
2024-04-30 14:26:07 +0100

    working! now using better suspend-to-ram commands

--- device/src/dash.sh
+++ device/src/dash.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env sh
+
+# This page is useful for low-level kindle operations: https://wiki.mobileread.com/wiki/Kindle_Touch_Hacking
+
+
 DEBUG=${DEBUG:-false}
 [[ "$DEBUG" = true ]] && set -x
 
-source "./logging.sh"
-
 DIR="$(dirname "$0")"
+
+source "$DIR/logging.sh"
 
 # Source environment variables
 ENV_FILE="$DIR/local/env.sh"
@@ -17,19 +21,100 @@ LOW_BATTERY_CMD="$DIR/local/low-battery.sh"
 # REFRESH_SCHEDULE=${REFRESH_SCHEDULE:-"2,32 8-17 * * MON-FRI"}
 FULL_DISPLAY_REFRESH_RATE=${FULL_DISPLAY_REFRESH_RATE:-0}
 SLEEP_SCREEN_INTERVAL=${SLEEP_SCREEN_INTERVAL:-3600}
-RTC=/sys/class/rtc/rtc1/wakealarm # for paperwhite 3
-# RTC=/sys/devices/platform/mxc_rtc.0/wakeup_enable # for earlier kindle devices
+
 
 LOW_BATTERY_REPORTING=${LOW_BATTERY_REPORTING:-false}
 LOW_BATTERY_THRESHOLD_PERCENT=${LOW_BATTERY_THRESHOLD_PERCENT:-10}
 
+
+DEVICE_TYPE=PW3
 num_refresh=0
+
+#===================== End of configuration =====================#
+
+# https://github.com/mattzzw/kindle-gphotos/blob/master/kindle-gphotos.sh
+# https://github.com/mattzzw/kindle-clock/blob/master/kindle-clock.sh
+FBROTATE=""
+BACKLIGHT=""
+RTC=""
+case $DEVICE_TYPE in
+    "K4")
+        FBROTATE="echo 14 2 > /proc/eink_fb/update_display"
+        BACKLIGHT="/dev/null"
+        # No RTC?
+        ;;
+    "PW2")
+        FBROTATE="echo -n 0 > /sys/devices/platform/mxc_epdc_fb/graphics/fb0/rotate"
+        BACKLIGHT="/sys/devices/system/fl_tps6116x/fl_tps6116x0/fl_intensity"
+        RTC=/sys/devices/platform/mxc_rtc.0/wakeup_enable
+        ;;
+    "PW3")
+        FBROTATE="echo 0 > /sys/devices/platform/imx_epdc_fb/graphics/fb0/rotate"
+        BACKLIGHT="/sys/devices/platform/imx-i2c.0/i2c-0/0-003c/max77696-bl.0/backlight/max77696-bl/brightness"
+        RTC=/sys/class/rtc/rtc1/wakealarm
+        ;;
+    *)
+        echo "Unrecognised device $DEVICE_TYPE. Must be K4, PW2 or PW3."
+        ;;
+esac
 
 optimise_power() {
   log_info "Optimising power usage."
-  initctl stop framework >/dev/null 2>&1
-  initctl stop webreader >/dev/null 2>&1
-  echo powersave >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+  
+  echo powersave >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor # put CPU into low-power mode
+  echo -n 0 > $BACKLIGHT # disable light. min=0, max=3280 (corresponding to 25 on the GUI slider)
+  
+  case $DEVICE_TYPE in
+    "K4")
+        /etc/init.d/framework stop
+        /etc/init.d/pmond stop
+        /etc/init.d/phd stop
+        /etc/init.d/cmd stop
+        /etc/initd./tmd stop
+        /etc/init.d/browserd stop
+        /etc/init.d/webreaderd stop
+        /etc/init.d/lipc-daemon stop
+        /etc/init.d/powerd stop
+        ;;
+    "PW2" | "PW3")
+        # TODO: which of these need doing? what about framework & webreader?
+        # TODO: use initctl stop instead of stop?
+        
+        # The framework job sends a SIGTERM on stop, trap it so we don't get killed if we were launched by KUAL
+        # https://www.mobileread.com/forums/showpost.php?p=2639195&postcount=5
+        trap "" SIGTERM
+        stop lab126_gui # main interface. #TODO: should this be framework instead?
+        usleep 1250000 # so we don't start before the blank screen
+        trap - SIGTERM
+        
+        stop webreader
+        
+        # OTA update related processes
+        # https://www.mobileread.com/forums/showpost.php?p=2422385&postcount=6
+        # https://www.mobileread.com/forums/showpost.php?p=2008593&postcount=13
+        stop otaupd # over-the-air update
+        stop phd # phone home
+        stop tmd # transfer manager
+        stop todo
+
+        # # Don't know if I should stop these...
+        # stop x 
+        # stop mcsd
+        # stop archive
+        # stop dynconfig
+        # stop dpmd 
+        # stop appmgrd # application manager
+        # stop stackdumpd
+        ;;
+    *)
+        echo "Unrecognised device $DEVICE_TYPE. Must be K4, PW2 or PW3."
+        ;;
+  esac
+  sleep 2
+  
+  ### turn off 270 degree rotation of framebuffer device. TODO: what is this? from kindle-clock
+  # eval $FBROTATE
+  
 }
 
 display_sleep_screen() {
@@ -45,20 +130,24 @@ display_sleep_screen() {
 }
 
 refresh_dashboard() {
+
+  # Re-enable wifi
+  lipc-set-prop com.lab126.cmd wirelessEnable 1
   "$DIR/wait-for-wifi.sh" "$WIFI_TEST_IP"
 
+  # Get image
   "$FETCH_DASHBOARD_CMD" "$DASH_PNG"
   fetch_status=$?
 
   if [ "$fetch_status" -ne 0 ]; then
     log_error "Not updating screen, fetch-dashboard returned $fetch_status"
+    /usr/sbin/eips "Error retrieving dashboard!"
     return 1
   fi
 
   if [ "$num_refresh" -eq "$FULL_DISPLAY_REFRESH_RATE" ]; then
-    num_refresh=0
-
     # trigger a full refresh once in every 4 refreshes, to keep the screen clean
+    num_refresh=0
     log_info "Refreshing: full"
     /usr/sbin/eips -f -g "$DASH_PNG" >/dev/null
   else
@@ -67,6 +156,9 @@ refresh_dashboard() {
   fi
 
   num_refresh=$((num_refresh + 1))
+
+  # Disable wifi
+  lipc-set-prop com.lab126.cmd wirelessEnable 0
 }
 
 log_battery_stats() {
@@ -87,14 +179,25 @@ rtc_sleep() {
   if [ "$DEBUG" = true ]; then
     sleep "$duration"
   else
-    content=$(cat "$RTC")  # Read the content of the file
-    if [ -z "$content" ] || [ "$content" -eq 0 ]; then  # Check if content is empty or zero
-      echo -n "$duration" >"$RTC"
-      echo "mem" >/sys/power/state
-    else
-      log_error "Couldn't use RTC; it contained $content."
-      exit 1
+    # lipc-set-prop -i com.lab126.powerd rtcWakeup "$duration" # doesn't seem to work. see https://www.mobileread.com/forums/showpost.php?p=3221077&postcount=7
+
+    if [ $duration -lt 5 ]; then
+        duration=60
     fi
+    
+    rtcwake -d /dev/rtc1 -m mem -s $duration
+    
+    # echo -n "$duration" >"$RTC"
+    # echo "mem" >/sys/power/state # suspend to RAM
+
+    # content=$(cat "$RTC")  # Read the content of the file
+    # if [ -z "$content" ] || [ "$content" -eq 0 ]; then  # Check if content is empty or zero
+    #   echo -n "$duration" >"$RTC"
+    #   echo "mem" >/sys/power/state
+    # else
+    #   log_error "Couldn't use RTC; it contained $content."
+    #   exit 1
+    # fi
   fi  
 }
 
@@ -131,8 +234,6 @@ if [ $# -eq 0 ]; then
     exit 1
   fi
 
-
-  
   lipc-set-prop com.lab126.powerd preventScreenSaver 1
   if [ "$DEBUG" = false ]; then
 
--- device/src/wait-for-wifi.sh
+++ device/src/wait-for-wifi.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env sh
 test_ip=$1
 
-source ./logging.sh
+DIR="$(dirname "$0")"
+
+source "$DIR/logging.sh"
 
 if [ -z "$test_ip" ]; then
     log_error -l ERROR "No test ip specified"
     exit 1
 fi
+
+# TODO: neither of these are used yet
+wait_wlan_connected() {
+  # shellcheck disable=SC2046
+  return $(lipc-get-prop com.lab126.wifid cmState | grep CONNECTED | wc -l)
+}
+
+wait_wlan_ready() {
+  # shellcheck disable=SC2046
+  return $(lipc-get-prop com.lab126.wifid cmState | grep -e READY -e PENDING -e CONNECTED | wc -l)
+}
 
 wait_for_wifi() {
     max_retry=30
